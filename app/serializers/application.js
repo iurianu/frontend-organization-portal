dc93d9f61d9ef65dba7fa4bb569ca99791566880
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-07-29 18:32:42 +0200

    Strip relationship links from the payloads
    This hides the huge amount of warnings Ember Data triggers.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -4,4 +4,33 @@ import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default class ApplicationSerializer extends JSONAPISerializer.extend(
   DataTableSerializerMixin
-) {}
+) {
+  normalize() {
+    let hash = super.normalize(...arguments);
+
+    return removeRelationshipLinks(hash);
+  }
+}
+
+// Ember Data shows a lot of warnings when it receives relationship links
+// for relationships marked as sync. To prevent that we simply strip that data from the payload.
+// This shouldn't be an issue since we include the needed relationships already.
+// More information: https://discuss.emberjs.com/t/ember-data-jsonapi-and-known-to-be-empty-relationships-that-are-definitely-not-empty/16219
+function removeRelationshipLinks(hash) {
+  if (hash.data && hash.data.relationships) {
+    let relationshipIdentifiers = hash.data.relationships;
+    let relationshipNames = Object.keys(relationshipIdentifiers);
+
+    relationshipNames.forEach((relationshipName) => {
+      let relationshipIdentifier = relationshipIdentifiers[relationshipName];
+
+      if (!relationshipIdentifier.data) {
+        delete relationshipIdentifiers[relationshipName];
+      } else if (relationshipIdentifier.links) {
+        delete relationshipIdentifier.links;
+      }
+    });
+  }
+
+  return hash;
+}
